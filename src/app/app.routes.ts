6d0bb0b933b6b247307856dfc11c44f4441fb1f7
Divya Teja Reddy Tadi <144609147+tejareddytadi@users.noreply.github.com>
2024-11-24 21:59:42 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ViewCartComponent } from './view-cart/view-cart.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'view-cart', component: ViewCartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'register', component: RegisterComponent }
+
+];
